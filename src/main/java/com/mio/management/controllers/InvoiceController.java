c2eb70e753e957faa73ceb2e2f37d93369f18f24
Michał <michal@hand2band.media>
2019-03-07 22:07:09 +0100

    new find methods byId, byIsPaid, byNumber, byBuyerName

--- src/main/java/com/mio/management/controllers/InvoiceController.java
+++ src/main/java/com/mio/management/controllers/InvoiceController.java
@@ -2,11 +2,10 @@ package com.mio.management.controllers;
 
 import com.mio.management.db.InvoiceRepository;
 import com.mio.management.models.Invoice;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/invoices")
@@ -20,6 +19,54 @@ public class InvoiceController {
     @GetMapping("/all")
     public List<Invoice> getAll(){
         List<Invoice> invoices = this.invoiceRepository.findAll();
+
+        return invoices;
+    }
+
+    @PutMapping
+    public void insert(@RequestBody Invoice invoice){
+        this.invoiceRepository.insert(invoice);
+    }
+
+    @PostMapping
+    public void update(@RequestBody Invoice invoice){
+        this.invoiceRepository.save(invoice);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") String id){
+        this.invoiceRepository.deleteById(id);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Invoice> getById(@PathVariable("id") String id){
+        Optional<Invoice> invoice = this.invoiceRepository.findById(id);
+
+        return invoice;
+    }
+
+    @GetMapping("/ispaid/{isPaid}")
+    public List<Invoice> getByIsPaid(@PathVariable("isPaid") boolean isPaid){
+        List<Invoice> invoices;
+        if (isPaid){
+            invoices = this.invoiceRepository.findByIsPaidIsTrue();
+        } else {
+            invoices = this.invoiceRepository.findByIsPaidIsFalse();
+        }
+
+        return invoices;
+    }
+
+    @GetMapping("/number/{number}")
+    public List<Invoice> getByNumber(@PathVariable("number") String number){
+        List<Invoice> invoices = this.invoiceRepository.findByNumber(number);
+
+        return invoices;
+    }
+
+    @GetMapping("/buyer/{name}")
+    public List<Invoice> getByBuyerName(@PathVariable("name") String name){
+        List<Invoice> invoices = this.invoiceRepository.findByBuyerName(name);
 
         return invoices;
     }
--- src/main/java/com/mio/management/db/InvoiceRepository.java
+++ src/main/java/com/mio/management/db/InvoiceRepository.java
@@ -2,8 +2,23 @@ package com.mio.management.db;
 
 import com.mio.management.models.Invoice;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface InvoiceRepository extends MongoRepository<Invoice,String> {
+
+    Optional<Invoice> findById(String id);
+
+    List<Invoice> findByNumber(String number);
+
+    List<Invoice> findByIsPaidIsTrue();
+
+    List<Invoice> findByIsPaidIsFalse();
+
+    @Query(value = "{'buyer.name':?0}")
+    List<Invoice> findByBuyerName(String name);
 }
